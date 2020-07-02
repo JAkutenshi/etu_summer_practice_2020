f6934a93bb81fd3f4d199ab74439f6cb5094cf07
JAkutenshi <jakutenshi@gmail.com>
2020-07-02 10:54:29 +0300

    Add submodules and intro task remote-fetch scripts

---
+++ fetch.sh
@@ -0,0 +1,9 @@
+# intro task
+git fetch AhimasYD
+git fetch rakhele
+git fetch sinelnikovmaxim
+git fetch Pavel-by
+git fetch pajecawav
+git fetch otsheln1k
+git fetch scissorsnatcher
+git fetch AlexRyzhickov
---
+++ remotes.sh
@@ -0,0 +1,9 @@
+# intro task
+git remote add AhimasYD         https://github.com/AhimasYD/etu_summer_practice_2020.git
+git remote add rakhele          https://github.com/rakhele/etu_summer_practice_2020.git
+git remote add sinelnikovmaxim  https://github.com/sinelnikovmaxim/etu_summer_practice_2020.git
+git remote add Pavel-by         https://github.com/Pavel-by/etu_summer_practice_2020.git
+git remote add pajecawav        https://github.com/pajecawav/etu_summer_practice_2020.git
+git remote add otsheln1k        https://github.com/otsheln1k/etu_summer_practice_2020.git
+git remote add scissorsnatcher  https://github.com/scissorsnatcher/etu_summer_practice_2020.git
+git remote add AlexRyzhickov    https://github.com/AlexRyzhickov/etu_summer_practice_2020.git
